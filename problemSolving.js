6d228fd27a30a851be228b01754cc0b800db4a27
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2025-02-19 19:11:14 +0530

    checkAnagram

--- problemSolving.js
+++ problemSolving.js
@@ -1,8 +1,5 @@
 const input1  = [1,2,3,2,1,1];
 const input2  = [4,1,9,5,1,4,90,1];
-const input = [];
-input.push(input1); input.push(input2);
-console.log(input);
 
 function frequencyCounters(){
     if (input1.length <= input2.length){
@@ -36,4 +33,18 @@ function frequencyCounterBetter(){
     }
 }
 
-console.log(frequencyCounterBetter()) // O(3n) == O(n)
+//console.log(frequencyCounterBetter()) // O(3n) == O(n)
+
+
+const checkAnagram = (str1,str2) =>{
+    console.log(str1,str2);
+    for(let i of str1.toLowerCase()){
+        if(!str2?.includes(i)) return false;
+        str2 = str2.replace(i,'');
+    }
+    console.log(str1,str2);
+    return true;
+}
+
+console.log(checkAnagram('anagram', 'nagarawm'));
+
