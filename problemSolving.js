d1e33b6e6ed6fe94e08885f63b7ddfbc9cde2930
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2025-02-24 11:47:08 +0530

    verify same frequency

--- problemSolving.js
+++ problemSolving.js
@@ -52,17 +52,14 @@ function countUniqueValues(arr) {
   if (arr.length > 1) {
     let counter = 0;
     let endCounter = 1;
-    while (arr.length>=endCounter) {
+    while (arr.length>endCounter) {
       if (arr[counter] != arr[endCounter]) {
         counter++;
         arr[counter] = arr[endCounter];
-        endCounter++;
-      } else {
-        endCounter++;
       }
+      endCounter++;
     }
-    console.log(counter)
-    return counter;
+    return counter+1;
     } else if (arr.length == 1) {
         return 1;
     } else {
@@ -70,4 +67,29 @@ function countUniqueValues(arr) {
     }
 }
 
-console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]));
+//console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]));
+
+
+function sameFrequency(num1,num2){
+  // good luck. Add any arguments you deem necessary.
+  const str1 = num1.toString();
+  const str2 = num2.toString();
+  const countObj1 = {}; const countObj2 = {};
+  for(let val of str1){
+      countObj1[val] = (countObj1[val] || 0) +1;
+  }
+  
+  for(let val of str2){
+      countObj2[val] = (countObj2[val] || 0) +1;
+  }
+  
+  for(let val of Object.keys(countObj1)){
+      if(countObj1[val] != countObj2[val]) return false;
+  }
+  return true;
+}
+
+console.log(sameFrequency(182,281)) // true
+console.log(sameFrequency(34,14)) // false
+console.log(sameFrequency(3589578, 5879385)) // true
+console.log(sameFrequency(22,222)) // false
