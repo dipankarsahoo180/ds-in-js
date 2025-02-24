4ef7af25efdf2bb34458f7b29b14832452010f2d
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2025-02-24 17:41:25 +0530

    find average pair

--- problemSolving.js
+++ problemSolving.js
@@ -136,7 +136,30 @@ function constructNote(str1,str2){
   return true
 }
 
-console.log(constructNote('aa', 'abc'), // false
-constructNote('abc', 'dcba'), // true
-constructNote('aabbcc', 'bcabcaddff'), // true
+// console.log(constructNote('aa', 'abc'), // false
+// constructNote('abc', 'dcba'), // true
+// constructNote('aabbcc', 'bcabcaddff'), // true
+// )
+
+function averagePair(arr, val){
+  let start=0;let end=arr.length-1;
+  if(arr.length>0){
+    while(start<=end){
+      const result = (arr[start]+ arr[end])/2; //console.log(start,end,result,val)
+      if(result == val) return true
+      else if(result > val){
+        end--;
+      }else{
+        start++;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(
+  averagePair([1,2,3],2.5), // true
+  averagePair([1,3,3,5,6,7,10,12,19],8), // true
+  averagePair([-1,0,3,4,5,6], 4.1), // false
+  averagePair([],4) // false
 )
