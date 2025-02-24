58683e3d0b35d0bb0d6c06ce27c604067eed1c09
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2025-02-25 00:02:08 +0530

    findLongestSubstringOptimal

--- problemSolving.js
+++ problemSolving.js
@@ -307,12 +307,30 @@ function findLongestSubstring(str){
   return {longestStringCount,longestStringValue};
 }
 
+function findLongestSubstringOptimal(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+ 
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1);
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
+  }
+  return longest;
+}
+
 console.log(
-  findLongestSubstring(''), // 0
-  findLongestSubstring('rithmschool'), // 7
-  findLongestSubstring('thisisawesome'), // 6
-  findLongestSubstring('thecatinthehat'), // 7
-  findLongestSubstring('bbbbbb'), // 1
-  findLongestSubstring('longestsubstring'), // 8
-  findLongestSubstring('thisishowwedoit'), // 6
+  findLongestSubstringOptimal(''), // 0
+  findLongestSubstringOptimal('rithmschool'), // 7
+  findLongestSubstringOptimal('thisisawesome'), // 6
+  findLongestSubstringOptimal('thecatinthehat'), // 7
+  findLongestSubstringOptimal('bbbbbb'), // 1
+  findLongestSubstringOptimal('longestsubstring'), // 8
+  findLongestSubstringOptimal('thisishowwedoit'), // 6
 )
