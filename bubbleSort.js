16d6876b12ccfa2d33f9d27b032b12ed6338bbf5
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2025-03-01 12:29:01 +0530

    bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,7 +2,7 @@ const bubbleSort = (arr) => {
     let noSwaps; //optimization
     for(let i=0;i<arr.length;i++){
         noSwaps = true;
-        for(let j=0;j<arr.length-i;j++){
+        for(let j=0;j<arr.length-i-1;j++){
             if(arr[j]>arr[j+1]){
                 // const temp = arr[j];
                 // arr[j] = arr[j+1];
