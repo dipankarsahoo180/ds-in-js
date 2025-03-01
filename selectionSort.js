50849dff4c719a7f3fc1b79b644553aeb6496f89
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2025-03-01 14:41:46 +0530

    selectionSortWithCompare

--- selectionSort.js
+++ selectionSort.js
@@ -8,7 +8,7 @@ function selectionSort(arr){
                 index = j+1
             }
         }
-        // console.log(i,arr[i],arr[index],min,arr);
+        // //console.log(i,arr[i],arr[index],min,arr);
         if(min<arr[i]){
             arr[index] = arr[i];
             arr[i] = min;
@@ -19,20 +19,17 @@ function selectionSort(arr){
 
 function selectionSortCmp(arr,comparator){
     if(typeof comparator != 'function'){
-        comparator = (a,b) => b-a;
+        comparator = (a,b) => a-b;
     }
 
     for(let i=0;i<arr.length;i++){
         let lowestIndexValue = i;
         for(let j=i+1;j<arr.length;j++){
-            if(comparator(arr[j],arr[lowestIndexValue])>=0){
+            if(comparator(arr[j],arr[lowestIndexValue])<0){
                 lowestIndexValue = j
             }
         }
-        // console.log(i,arr[i],arr[lowestIndexValue],arr);
-        const temp = arr[i];
-        arr[i] = arr[lowestIndexValue];
-        arr[lowestIndexValue] = temp;
+        if(lowestIndexValue!=i)[arr[i],arr[lowestIndexValue]] = [arr[lowestIndexValue],arr[i]];
         
     }
     return arr;
@@ -60,22 +57,22 @@ return b.age - a.age;
 const arrr = selectionSortCmp(moarKittyData, oldestToYoungest); // sorted by age in descending order
 console.log(arrr)
 
-console.log(
-    selectionSort([3,4,6,1,1,2,3,4,7]),
-    selectionSort([3,4,6,12,8,1,2,3,4]),
-    selectionSort([1,56,23,2,3,3,4,6,1,1,2,3,4]),
-    selectionSort([4])
-)
+//console.log(
+    // selectionSort([3,4,6,1,1,2,3,4,7]),
+    // selectionSort([3,4,6,12,8,1,2,3,4]),
+    // selectionSort([1,56,23,2,3,3,4,6,1,1,2,3,4]),
+    // selectionSort([4])
+// )
 
-console.log(
-selectionSortCmp([4, 20, 12, 10, 7, 9]), // [4, 7, 9, 10, 12, 20]
-selectionSortCmp([0, -10, 7, 4]), // [-10, 0, 4, 7]
-selectionSortCmp([1, 2, 3]), // [1, 2, 3]
-selectionSortCmp([]),
-)
+//console.log(
+// selectionSortCmp([4, 20, 12, 10, 7, 9]), // [4, 7, 9, 10, 12, 20]
+// selectionSortCmp([0, -10, 7, 4]), // [-10, 0, 4, 7]
+// selectionSortCmp([1, 2, 3]), // [1, 2, 3]
+// selectionSortCmp([]),
+// )
  
 var nums = [4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32];
-console.log(selectionSortCmp(nums)); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67, 75, 232, 232, 453, 546, 4342]
+//console.log(selectionSortCmp(nums)); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67, 75, 232, 232, 453, 546, 4342]
  
 var kitties = ["LilBub", "Garfield", "Heathcliff", "Blue", "Grumpy"];
  
