0ca2df5d8b20328241de61cefb37fde448bb5a21
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2025-03-08 21:05:58 +0530

    practice

--- arrayTest.js
+++ arrayTest.js
@@ -4,3 +4,11 @@ arr.splice(1,2,6); //start index,delete count,replace with
 console.log(arr)
 arr = arr.slice(1,3) //include index,exclude index
 console.log(arr)
+
+
+
+
+console.log(...[[1.3,3434350989,34878905,213,23],[1, 56, 23, 2, 3, 3, 4, 6, 1, 1, 2, 3, 4]])
+console.log([].concat(...[1.3,3434350989,34878905,213,23,[1,2,34,[1,2,3,[1.2,3]]]]))
+console.log(...[[1.3,3434350989,34878905,213,23,[1,2,34]],[1, 56, 23, 2, 3, 3, 4, 6, 1, 1, 2, 3, 4]])
+console.log([].concat(...[[1.3,3434350989,34878905,213,23,[1,2,34]],[1, 56, 23, 2, 3, 3, 4, 6, 1, 1, 2, 3, 4]]))
