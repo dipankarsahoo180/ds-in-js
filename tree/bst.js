4a7b162f978517efb581779bb91bfb4932e8a46d
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2025-03-16 23:29:50 +0530

    binary search tree

--- tree/bst.js
+++ tree/bst.js
@@ -79,6 +79,77 @@ class BinarySearchTree {
         }
         return node;
     }
+
+    deleteIteratively(value) {
+        if (!this.root) return null;
+    
+        let parent = null;
+        let current = this.root;
+    
+        // Find the node to be deleted and its parent
+        while (current && current.value !== value) {
+            parent = current;
+            if (value < current.value) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+    
+        // Node not found
+        if (!current) return null;
+    
+        // Case 1: Node to be deleted has no children (leaf node)
+        if (!current.left && !current.right) {
+            if (current === this.root) {
+                this.root = null;
+            } else if (current === parent.left) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        }
+        // Case 2: Node to be deleted has one child
+        else if (!current.left) {
+            if (current === this.root) {
+                this.root = current.right;
+            } else if (current === parent.left) {
+                parent.left = current.right;
+            } else {
+                parent.right = current.right;
+            }
+        } else if (!current.right) {
+            if (current === this.root) {
+                this.root = current.left;
+            } else if (current === parent.left) {
+                parent.left = current.left;
+            } else {
+                parent.right = current.left;
+            }
+        }
+        // Case 3: Node to be deleted has two children
+        else {
+            // Find the in-order successor (smallest in the right subtree)
+            let successorParent = current;
+            let successor = current.right;
+            while (successor.left) {
+                successorParent = successor;
+                successor = successor.left;
+            }
+    
+            // Replace current node's value with successor's value
+            current.value = successor.value;
+    
+            // Delete the successor node
+            if (successorParent.left === successor) {
+                successorParent.left = successor.right;
+            } else {
+                successorParent.right = successor.right;
+            }
+        }
+    
+        return this.root;
+    }
     
     minValueNode(node) {
         while (node.left) {
